7d3342a619936bf63fc9e892938857c36a6a5fbf
sagorkala93-eng <sagorkala93@gmail.com>
2025-12-07 14:21:08 +0600

    Update Admin2.js

--- scripts/commands/Admin2.js
+++ scripts/commands/Admin2.js
@@ -20,7 +20,7 @@ module.exports.config = {
 
 module.exports.run = async function ({ api, event }) {
   const currentTime = moment.tz("Asia/Dhaka").format("DD MMM YYYY, hh:mm:ss A");
-  const imageUrl = "https://graph.facebook.com/61583072616904/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
+  const imageUrl = "https://graph.facebook.com/61556298365773/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
   const imgPath = __dirname + "/cache/admin_avatar.png";
 
   const infoText = `
@@ -35,7 +35,7 @@ module.exports.run = async function ({ api, event }) {
 📧 𝗘𝗺𝗮𝗶𝗹: sumonislam124@gmail.com
 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽: +8801975257710
 ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺: https://t.me/sumonvpn
-🔗 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: fb.com/61583072616904
+🔗 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸: fb.com/61556298365773
 ⏰ 𝗧𝗶𝗺𝗲: ${currentTime}
 ╰╼|━━━━━━━━━━━━━━|╾╯`;
 
