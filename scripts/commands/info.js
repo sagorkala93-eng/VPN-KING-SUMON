2dab9a73412c08cfe23f8286961a7333b35c8236
sagorkala93-eng <sagorkala93@gmail.com>
2025-12-07 14:27:29 +0600

    Update Facebook profile link and image URL

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -23,7 +23,7 @@ module.exports.config = {
 module.exports.run = async function ({ api, event }) {
   const currentTime = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
 
-  const imageUrl = "https://graph.facebook.com/61583072616904/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
+  const imageUrl = "https://graph.facebook.com/61556298365773/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
   const imgPath = __dirname + "/cache/info_avatar.png";
 
   const infoText = `
@@ -76,7 +76,7 @@ module.exports.run = async function ({ api, event }) {
 ╰╼|━━━━━━━━━━━━━━|╾╯
 
 ╭╼|━━━━━━━━━━━━━━|╾╮
-🔗 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗟𝗶𝗻𝗸 :https://www.facebook.com/profile.php?id=61583072616904
+🔗 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗟𝗶𝗻𝗸 :https://www.facebook.com/profile.php?id=61556298365773
 ╰╼|━━━━━━━━━━━━━━|╾╯
 
 ╭╼|━━━━━━━━━━━━━━|╾╮
